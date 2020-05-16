7ad5a66d38d8f6d82db75824dfa41f44b41104a5
Salim Chehab <salimchehab@gmail.com>
2020-05-16 21:31:52 +0200

    Adds bash script to build the image and test the versions

---
+++ run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+readonly CONTAINER_IMAGE=devops-tools
+readonly TAG="1.0.0"
+
+function build() {
+    sudo docker build --tag "${CONTAINER_IMAGE}:${TAG}" .
+}
+
+function verify_versions() {
+    sudo docker run -i ${CONTAINER_IMAGE}:${TAG} terraform -v
+    sudo docker run -i ${CONTAINER_IMAGE}:${TAG} vault -v
+    sudo docker run -i ${CONTAINER_IMAGE}:${TAG} aws --version
+    sudo docker run -i ${CONTAINER_IMAGE}:${TAG} python3.7 --version
+    sudo docker run -i ${CONTAINER_IMAGE}:${TAG} salt --version
+    sudo docker run -i ${CONTAINER_IMAGE}:${TAG} ansible --version
+}
+
+function main() {
+    build
+    verify_versions
+}
+
+main
